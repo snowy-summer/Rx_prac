e8e846391283ea3a46548bf1e2dbb1f815ad1019
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-08-02 22:51:32 +0900

    [Prac] birthDayView, PhoneView rx 연습

--- SeSACRxThreads/SignUpOptional/BirthdayViewController.swift
+++ SeSACRxThreads/SignUpOptional/BirthdayViewController.swift
@@ -6,7 +6,17 @@
 //
  
 import UIKit
+import RxSwift
+import RxCocoa
 import SnapKit
+
+/*
+ 화면 진입시 오늘 날짜 데이트 피커와 레이블에 보여주기
+ 데이트 피커에서 선택한 날짜로 나이 계산했을 때, 만 17세 이상이 아니라면 infoLabel에 만 17세 이상만 가입 가능하다고 보여주기
+ 글자 빨간색, 버튼 lightGray 클릭 안됨
+ 조건 만족되면 "가입 가능한 나이입니다" 텍스트와 파란 글씨로 변경, 버튼은 파란색
+ 클릭시 완료 alert
+ */
 
 class BirthdayViewController: UIViewController {
     
@@ -66,26 +76,75 @@ class BirthdayViewController: UIViewController {
   
     let nextButton = PointButton(title: "가입하기")
     
+    private let date = BehaviorRelay(value: Date())
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = Color.white
         
         configureLayout()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+    
+        bind()
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(SearchViewController(), animated: true)
+    private func bind() {
+        
+        birthDayPicker.rx.date
+            .bind(to: date)
+            .disposed(by: disposeBag)
+        
+        date.bind(with: self) { owner, value in
+            let component = Calendar.current.dateComponents([.year,.month,.day], from: value)
+            
+            owner.yearLabel.text = "\(component.year!) 년"
+            owner.monthLabel.text = "\(component.month!) 월"
+            owner.dayLabel.text = "\(component.day!) 일"
+        }
+        .disposed(by: disposeBag)
+        
+        date.map {
+            Calendar.current.dateComponents([.year], from: $0, to: Date()).year! >= 17
+        }.bind(with: self) { owner, value in
+            owner.infoLabel.text = value ? "가입 가능한 나이입니다." : "만 17세 이상만 가입 가능합니다."
+        }
+        .disposed(by: disposeBag)
+        
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.showAlert()
+            }
+            .disposed(by: disposeBag)
+        
     }
 
+    private func showAlert() {
+        
+        let profileAlert = UIAlertController(title: "완료",
+                                             message: "완~료",
+                                             preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "확인",
+                                          style: .cancel)
+        let cancelAction = UIAlertAction(title: "취소",
+                                         style: .default)
+        
+        profileAlert.addAction(confirmAction)
+        profileAlert.addAction(cancelAction)
+        
+        self.present(profileAlert,
+                     animated: false)
+    }
     
     func configureLayout() {
         view.addSubview(infoLabel)
         view.addSubview(containerStackView)
         view.addSubview(birthDayPicker)
         view.addSubview(nextButton)
+        
+        birthDayPicker.date = Date()
  
         infoLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(150)
--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -6,12 +6,21 @@
 //
  
 import UIKit
+import RxSwift
+import RxCocoa
 import SnapKit
+
+/*
+ 첫화면 진입 010 텍스트 필드에 보여주기
+ 텍스트 필드에 숫자만 가능, 10자 이상
+ 텍스트 필드 조건이 안맞으면 처리
+ */
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +30,23 @@ class PhoneViewController: UIViewController {
         configureLayout()
         
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        
+        bind()
     }
+    
+    private func bind() {
+        
+        phoneTextField.rx.text.orEmpty
+            .map {
+               let num = Int($0) ?? 0
+                return num != 0 && String(num).count >= 10
+            }
+            .bind(with: self) { owner, value in
+                owner.nextButton.isHidden = !value
+            }
+            .disposed(by: disposeBag)
+    }
+    
     
     @objc func nextButtonClicked() {
         navigationController?.pushViewController(NicknameViewController(), animated: true)
@@ -31,7 +56,8 @@ class PhoneViewController: UIViewController {
     func configureLayout() {
         view.addSubview(phoneTextField)
         view.addSubview(nextButton)
-         
+        
+        phoneTextField.text = "010"
         phoneTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
